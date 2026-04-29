0c19b1c777ffc92c29755f46d5320bbed9e7a0ed
rayn-zzz <rayn_zhang@outlook.com>
2026-04-29 11:55:19 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+# 数据集文件夹
+DATASET_PATH = "/home/dataset"
+# aisbench 工作路径, 为 git clone aisbench 后得到的 benchmark 目录的绝对路径
+# 可通过命令 `pip show ais-bench-benchmark` 查询location
+WORK_PATH = "/home/benchmark"
+# 服务化配置的模型名称
+MODEL_NAME = "ds"
+# 模型权重路径, 用于读取 tokenizer
+MODEL_PATH = "/home/weights/model_weights"
+# 请求目的 IP
+HOST_IP = "141.xx.xx.xx"
+# 请求目的端口
+HOST_PORT = "8004"
+
+# 如果使用稳态测试请将该字段设置为 "stable_stage"
+DEFAULT_PERFORMANCE_TEST = "default_perf"
