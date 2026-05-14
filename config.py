6f799fc3d56d17c0bcdc8488337865d7ce415165
rayn-zzz <rayn_zhang@outlook.com>
2026-05-14 15:28:11 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
-# 数据集文件夹
+# 数据集文件夹路径，需可访问
 DATASET_PATH = "/home/dataset"
+
 # aisbench 工作路径, 为 git clone aisbench 后得到的 benchmark 目录的绝对路径
 # 可通过命令 `pip show ais-bench-benchmark` 查询location
 WORK_PATH = "/home/benchmark"
+
 # 服务化配置的模型名称
 MODEL_NAME = "ds"
 # 模型权重路径, 用于读取 tokenizer
@@ -17,3 +19,9 @@ DEFAULT_PERFORMANCE_TEST = "default_perf"
 
 # aisbench输出日志保存路径
 OUTPUT_DIR = "./outputs/default"
+
+# 各节点信息，格式为 ["{ip}:{port}"]
+# 用于查询vllm metrics计算各个dp域的prefix cache命中率，不配置默认为HOST_IP:HOST_PORT
+# PD分离场景请填写各个节点的IP和port
+# POD_INFO = ["141.xx.xx.11:8000","141.xx.xx.12:8000"]
+POD_INFO = []
